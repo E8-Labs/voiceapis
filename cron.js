db76de6e7a51666a8224c3fe32fabcd4f48a1a09
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-08-10 23:14:54 +0500

    some updates

--- cron.js
+++ cron.js
@@ -2,28 +2,57 @@ import nodeCron from 'node-cron'
 import db from "./src/models/index.js";
 import { GetACall } from './src/controllers/call.controller.js';
 
-async function getCallsAndDetails(){
-    console.log("Running cron job")
-    let calls = await db.CallModel.findAll({
-        where:{
-            [db.sequelize.Op.or]: [
-                {status: {[db.sequelize.Op.ne]: "completed"}}
-            ]
-        }
-    })
-    console.log("Calls ", calls)
-    if(calls && calls.length > 0){
-        console.log("Pending calls found ")
-        for(let i = 0; i < calls.length; i++){
-            let callId = calls[i].callId;
-            console.log("Getting call id ", callId);
-            let data = GetACall(callId)
-            console.log("Call fetched and updated ", data)
-        }
-    }
-}
+// async function getCallsAndDetails(){
+//     console.log("Running cron job")
+//     let calls = await db.CallModel.findAll({
+//         where:{
+//             [db.sequelize.Op.or]: [
+//                 {status: {[db.sequelize.Op.ne]: "completed"}}
+//             ]
+//         }
+//     })
+//     console.log("Calls ", calls)
+//     if(calls && calls.length > 0){
+//         console.log("Pending calls found ")
+//         for(let i = 0; i < calls.length; i++){
+//             let callId = calls[i].callId;
+//             console.log("Getting call id ", callId);
+//             let data = GetACall(callId)
+//             console.log("Call fetched and updated ", data)
+//         }
+//     }
+// }
 
 //every two min
+
+async function getCallsAndDetails() {
+    console.log("Running cron job");
+    try {
+        let calls = await db.CallModel.findAll({
+            where: {
+                status: { [db.Sequelize.Op.ne]: "completed" }
+            }
+        });
+        console.log("Calls found: ", calls.length);
+
+        if (calls && calls.length > 0) {
+            console.log("Pending calls found");
+            for (let i = 0; i < calls.length; i++) {
+                let callId = calls[i].callId;
+                console.log("Getting call id ", callId);
+                
+                try {
+                    let data = await GetACall(callId);
+                    console.log("Call fetched and updated: ", data);
+                } catch (error) {
+                    console.error(`Error fetching call with id ${callId}:`, error);
+                }
+            }
+        }
+    } catch (error) {
+        console.error("Error fetching calls:", error);
+    }
+}
 const job = nodeCron.schedule('*/1 * * * *', getCallsAndDetails)
 
 job.start();
