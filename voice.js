c31c01df3dc2569995accafd1f0d1fa08812ab24
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-08-21 10:33:56 +0500

    some updates

--- voice.js
+++ voice.js
@@ -20,20 +20,20 @@ app.use(express.json())
 
 
 
-// app.use(cors({
-//     origin: 'https://voiceai-ruby.vercel.app',
-//     methods: ['GET', 'POST'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-//   }));
+app.use(cors({
+    origin: 'http://localhost:3000',//https://voiceai-ruby.vercel.app
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }));
   
-//   app.options('*', (req, res) => {
-//     res.header('Access-Control-Allow-Origin', 'https://voiceai-ruby.vercel.app');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.sendStatus(200);
-//   });
+  app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
+  });
 
 db.sequelize.sync({alter: true})
 
