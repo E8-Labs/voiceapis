27555078c14662bc9e5f4b024deb1c063902512a
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-08-23 22:00:04 +0500

    some updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -104,13 +104,14 @@ function generateRandomCode(length) {
 
 export const SendPhoneVerificationCode = async (req, res) => {
     let phone = req.body.phone;
+    let login = req.body.login || false
     let user = await db.User.findOne({
         where: {
             phone: phone
         }
     })
     //console.log("User is ", user)
-    if (user) {
+    if (user && !login) {
         res.send({ status: false, data: null, message: "Phone already taken" })
     }
     else {
