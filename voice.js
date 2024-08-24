4d28b1360490526957c78f3ceb9299fee58a54e0
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-08-24 21:14:02 +0500

    some updates

--- voice.js
+++ voice.js
@@ -21,14 +21,14 @@ app.use(express.json())
 
 
 app.use(cors({
-    origin: 'https://voiceai-ruby.vercel.app',//https://voiceai-ruby.vercel.app
+    origin: 'http://localhost:8005',//https://voiceai-ruby.vercel.app
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
   
   app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://voiceai-ruby.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8005');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
