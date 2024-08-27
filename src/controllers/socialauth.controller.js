550e309070b939f30b53d5f36b3e2ebb7747792e
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-08-27 14:35:46 +0500

    some updates

--- src/controllers/socialauth.controller.js
+++ src/controllers/socialauth.controller.js
@@ -1,6 +1,7 @@
 import db from "../models/index.js";
 import S3 from "aws-sdk/clients/s3.js";
 import JWT from "jsonwebtoken";
+import qs  from 'qs';
 import bcrypt from 'bcryptjs';
 import multer from "multer";
 import path from "path";
@@ -17,19 +18,24 @@ const User = db.User;
 const Op = db.Sequelize.Op;
 
 export const AddInstagramAuth = async (req, res) => {
-    console.log("Add instagram login api", req.body.clientId)
+    console.log("Add instagram login api", process.env.InstaClientId)
     JWT.verify(req.token, process.env.SecretJwtKey, async (error, authData) => {
         if (authData) {
             let user = await db.User.findByPk(authData.user.id);
             try {
                 const response = await axios.post(
                   `https://api.instagram.com/oauth/access_token`,
-                  {
+                  qs.stringify({
                     client_id: process.env.InstaClientId,
                     client_secret: process.env.InstaClientSecret,
                     grant_type: 'authorization_code',
                     redirect_uri: process.env.InstaredirectUri,
                     code: req.body.code,
+                  }),
+                  {
+                    headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded',
+                    },
                   }
                 );
                 const { access_token } = response.data;
