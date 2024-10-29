7a1839203f009f80c2b39387c148d6a742a5a12f
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-10-29 23:43:05 +0500

    custom action update

--- src/controllers/action.controller.js
+++ src/controllers/action.controller.js
@@ -1,7 +1,7 @@
 export const CheckCalendarAvailability = async (req, res) => {
   console.log("Hello in Custom action");
-  // return res.send({
-  //   status: true,
-  //   message: "Custom action Check Availability triggered",
-  // });
+  return res.send({
+    status: true,
+    message: "Custom action Check Availability triggered",
+  });
 };
